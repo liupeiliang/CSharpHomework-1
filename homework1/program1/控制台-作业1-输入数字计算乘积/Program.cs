84bcb85e26272ebfb9ecb9ba27e7b6f22acf63f0
zifeiyu <1186352914@qq.com>
2018-09-09 19:34:02 +0800

    第一次作业

---
+++ homework1/program1/控制台-作业1-输入数字计算乘积/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 控制台_作业1_输入数字计算乘积
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double num1 = 0;
+            double num2 = 0;
+            Console.WriteLine("请输入第一个数字：");
+            s = Console.ReadLine();
+            num1 = Double.Parse(s);
+            Console.WriteLine("请输入第二个数字：");
+            s = Console.ReadLine();
+            num2 = Double.Parse(s);
+            Console.WriteLine(num1 + "*" + num2 + " = " + (num1 * num2));
+        }
+    }
+}
---
+++ homework1/program2/Windows-作业1-输入两个数字计算乘积/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_作业1_输入两个数字计算乘积
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Num1, Num2, Result;
+            string text;
+            Num1 = Double.Parse(num1.Text);
+            Num2 = Double.Parse(num2.Text);
+            Result = Num1 * Num2;
+            text = Result.ToString();
+            result.Text = text;
+        }
+    }
+}
