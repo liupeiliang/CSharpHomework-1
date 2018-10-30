849c44028eb3d0d32c891a5b8d6eaec76c99741b
zifeiyu0531 <2017302650068@whu.edu.cn>
2018-10-30 20:42:57 +0800

    第七次作业

---
+++ homework7/homework6/OrderService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace homework6
+{
+    public class OrderService
+    {
+        public BindingList<Order> orderObject = new BindingList<Order>();
+        public void addOrder(string client, string name, int id, int price)
+        {
+            orderObject.Add(new Order(client, name, id, price));
+        }
+        public void deleteOrder(int id)
+        {
+            Order order = new Order("", "", 0, 0);
+            foreach (Order e in orderObject)
+            {
+                if (e.id == id)
+                {
+                    order = e;
+                }
+            }
+            orderObject.Remove(order);
+        }
+        public void changeOrder(int id, string newClient, string newName, int newId, int newPrice)
+        {
+            Order order = new Order("", "", 0, 0);
+            foreach (Order e in orderObject)
+            {
+                if (e.id == id)
+                {
+                    order = e;
+                }
+            }
+            orderObject.Remove(order);
+            orderObject.Add(new Order(newClient, newName, newId, newPrice));
+        }
+        public Order searchOrder(int id)
+        {
+            Order order = new Order("", "", 0, 0);
+            foreach(Order e in orderObject)
+            {
+                if(e.id == id)
+                {
+                    order = e;
+                }
+            }
+            return order;
+        }
+    }
+}
---
+++ homework7/homework7/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using homework6;
+namespace homework7
+{
+    public partial class Form2 : Form
+    {
+        Form1 form1 = new Form1();
+        public string client { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+        public int price { get; set; }
+        public Form2(BindingList<Order> orderObject)
+        {
+            InitializeComponent();
+            form1.service.orderObject = orderObject;
+            textBox1.DataBindings.Add("Text", this, "id");
+            textBox2.DataBindings.Add("Text", this, "name");
+            textBox3.DataBindings.Add("Text", this, "client");
+            textBox4.DataBindings.Add("Text", this, "price");
+            form1.orderBindingSource.DataSource = form1.service.orderObject;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form1.service.addOrder(client, name, id, price);
+        }
+    }
+}
